650d188d73f12cd7cf80d05e205720066c714791
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-05-07 13:01:06 +0300

    fixed Package build

--- Tests/UIKitExtensionsTests/UIKitExtensionsTests.swift
+++ Tests/UIKitExtensionsTests/UIKitExtensionsTests.swift
@@ -6,6 +6,5 @@ final class UIKitExtensionsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(UIKitExtensions().text, "Hello, World!")
     }
 }
